ccdd5f4ffa06729053c0eae9a586802b4e5f8a4e
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2024-02-04 13:02:06 -0500

    Refactor code for improved readability
    Updated the code to adopt a unified structure across files for better readability, and removed unnecessary files. This change doesn't affect functionality, but makes the code cleaner, more consistent, and easier to understand and maintain.

--- app/components/3d-lens/ThreeDLens.tsx
+++ app/components/3d-lens/ThreeDLens.tsx
@@ -1,50 +1,50 @@
 'use client'
-import { useEffect, useCallback, ReactNode, FC } from 'react';
+import {FC, ReactNode, useCallback, useEffect} from 'react';
 
 interface Props {
-  children: ReactNode;
+    children: ReactNode;
 }
 
-const ThreeDLens: FC<Props> = ({ children }) => {
-  let moveX: number | undefined;
-  let moveY: number | undefined;
+const ThreeDLens: FC<Props> = ({children}) => {
+    let moveX: number | undefined;
+    let moveY: number | undefined;
 
-  const eventListener = useCallback((ev: MouseEvent) => {
-    moveX = (ev.clientX - window.innerWidth / 2) * -0.005;
-    moveY = (ev.clientY - window.innerHeight / 2) * 0.01;
+    const eventListener = useCallback((ev: MouseEvent) => {
+        moveX = (ev.clientX - window.innerWidth / 2) * -0.005;
+        moveY = (ev.clientY - window.innerHeight / 2) * 0.01;
 
-    document.documentElement.style.setProperty('--move-x', `${moveX}deg`);
-    document.documentElement.style.setProperty('--move-y', `${moveY}deg`);
-  }, []);
+        document.documentElement.style.setProperty('--move-x', `${moveX}deg`);
+        document.documentElement.style.setProperty('--move-y', `${moveY}deg`);
+    }, []);
 
-  const throttledEventListener = useCallback(
-    (func: (ev: MouseEvent) => void, limit: number) => {
-      let inThrottle: boolean = false;
+    const throttledEventListener = useCallback(
+        (func: (ev: MouseEvent) => void, limit: number) => {
+            let inThrottle: boolean = false;
 
-      return function (this: Document, ev: MouseEvent) {
-        const context = this;
+            return function (this: Document, ev: MouseEvent) {
+                const context = this;
 
-        if (!inThrottle) {
-          func.call(context, ev);
-          inThrottle = true;
-          setTimeout(() => (inThrottle = false), limit);
-        }
-      };
-    },
-    []
-  );
+                if (!inThrottle) {
+                    func.call(context, ev);
+                    inThrottle = true;
+                    setTimeout(() => (inThrottle = false), limit);
+                }
+            };
+        },
+        []
+    );
 
-  useEffect(() => {
-    const handleMouseMove = throttledEventListener(eventListener, 10);
+    useEffect(() => {
+        const handleMouseMove = throttledEventListener(eventListener, 10);
 
-    document.addEventListener('mousemove', handleMouseMove);
+        document.addEventListener('mousemove', handleMouseMove);
 
-    return () => {
-      document.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, [eventListener, throttledEventListener]);
+        return () => {
+            document.removeEventListener('mousemove', handleMouseMove);
+        };
+    }, [eventListener, throttledEventListener]);
 
-  return <>{children}</>;
+    return <>{children}</>;
 };
 
 export default ThreeDLens;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,32 @@
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { ReactNode } from "react";
+import {ReactNode} from "react";
 
 interface Props {
-  children: ReactNode
+    children: ReactNode
 }
 
 const kamerik = localFont({
-  src: [
-    {
-      path: '../public/fonts/kamerik205-heavy.woff2',
-      weight: '900',
-    },
-    // {
-    //   path: '../public/fonts/merriweather-regular-italic.woff2',
-    //   weight: '400',
-    // },
-  ],
-});
+    src: '../public/fonts/kamerik205-heavy.woff2',
+    variable: '--font-kamerik'
+})
+const merriWeather = localFont({
+    src: '../public/fonts/merriweather-regular-italic.woff2',
+    variable: '--font-merriweather'
+})
 
 export const metadata: Metadata = {
-  title: "Natural Forest",
-  description: "3D Lens Effect",
+    title: "Natural Forest",
+    description: "3D Lens Effect",
 };
 
 export default function RootLayout({children}: Readonly<Props>) {
-  return (
-    <>
-      <html lang="en">
-        <body className={kamerik.className}>{children}</body>
-      </html>
-    </>
-  );
+    return (
+        <>
+            <html lang="en">
+            <body className={`${kamerik.variable} ${merriWeather.variable}`}>{children}</body>
+            </html>
+        </>
+    );
 }

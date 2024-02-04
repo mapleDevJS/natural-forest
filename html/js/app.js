082176638ec6df0e5703502637947ae915b67981
Alexey Ivanov <alexey.ivanov.js@gmail.com>
2024-02-03 23:31:03 -0500

    Refactor mousemove event handler in app.js
    The event listener of the mousemove event was refactored to improve performance. This was done by applying a throttle function to limit the frequency of style property updates, thus enhancing the efficiency of the application.

--- html/js/app.js
+++ html/js/app.js
@@ -1,8 +1,26 @@
-document.addEventListener('mousemove', e => {
-	Object.assign(document.documentElement, {
-		style: `
-		--move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
-		--move-y: ${(e.clientY - window.innerHeight / 2) * .01}deg;
-		`
-	})
-})
+let moveX, moveY;
+
+function eventListener(e) {
+	moveX = (e.clientX - window.innerWidth / 2) * -.005;
+	moveY = (e.clientY - window.innerHeight / 2) * .01;
+
+	document.documentElement.style.setProperty('--move-x', `${moveX}deg`);
+	document.documentElement.style.setProperty('--move-y', `${moveY}deg`);
+}
+const throttledEventListener = throttle(eventListener, 10);
+
+document.addEventListener('mousemove', throttledEventListener);
+
+// Throttle function
+function throttle(func, limit) {
+	let inThrottle;
+	return function() {
+		const args = arguments;
+		const context = this;
+		if (!inThrottle) {
+			func.apply(context, args);
+			inThrottle = true;
+			setTimeout(() => inThrottle = false, limit);
+		}
+	}
+}
